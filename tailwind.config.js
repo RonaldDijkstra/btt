3fb76d230b666c3f6ef68efa45213e98d4ecd1d2
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2023-07-23 23:24:36 +0200

    Add some assets, use them, change color palet

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,7 @@ module.exports = {
       transparent: 'transparent',
       white: colors.white,
       black: colors.black,
-      textgray: "#3f3f46",
-      gray: colors.gray,
-      red: colors.red,
-      green: {
-        default: "#54b848",
-        darker: "#3B9F2F",
-        dark: "#326f2b",
-      },
-      "border-gray": "#71717a",
-      "neutral": "#171717"
+      neutral: colors.trueGray,
     },
     // extend: {
     //   fontFamily: {
